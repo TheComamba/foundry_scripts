31431dce80fe5ce23ab87b6849737e04b0a2e91a
TheComamba <thecomamba@jojoheinze.de>
2025-05-04 20:11:13 +0200

    Assuming that this is cloned as a subrepo instead of a subtree.

--- update.sh
+++ update.sh
@@ -5,8 +5,8 @@ set -e
 # Change to git root directory
 cd "$(git rev-parse --show-toplevel)"
 
-# Update git subtree
-git subtree pull --prefix foundry_scripts git@github.com:TheComamba/FoundryScripts.git main --squash
-
 # Update npm packages
 npm update
+
+# Update git ssubmodule
+git submodule update --remote
