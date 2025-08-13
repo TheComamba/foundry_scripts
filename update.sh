92eb98036360cb38b1c72682fdb5b86898c82ae6
TheComamba <thecomamba@jojoheinze.de>
2025-08-13 17:59:03 +0200

    Making nvm available in script.

--- update.sh
+++ update.sh
@@ -8,6 +8,10 @@ cd "$(git rev-parse --show-toplevel)"
 # Update git submodule
 git submodule update --remote
 
+# Make nvm available in the shell
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 # Use latest node
 nvm use node
 
