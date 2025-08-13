18f12143e48c77e20310878da250978aaaa1ea46
TheComamba <thecomamba@jojoheinze.de>
2025-08-13 18:00:26 +0200

    Chaning order.

--- update.sh
+++ update.sh
@@ -5,9 +5,6 @@ set -e
 # Change to git root directory
 cd "$(git rev-parse --show-toplevel)"
 
-# Update git submodule
-git submodule update --remote
-
 # Make nvm available in the shell
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -17,3 +14,6 @@ nvm use node
 
 # Update npm packages
 npm update
+
+# Update git submodule (this is the last step, in order not to interfere with the script that is currently executed.)
+git submodule update --remote
