b280ffcd6bd718489ba441069345eb8c04cfd58b
TheComamba <thecomamba@jojoheinze.de>
2025-08-13 17:55:13 +0200

    Updating update script.

--- update.sh
+++ update.sh
@@ -5,8 +5,11 @@ set -e
 # Change to git root directory
 cd "$(git rev-parse --show-toplevel)"
 
+# Update git submodule
+git submodule update --remote
+
+# Use latest node
+nvm use node
+
 # Update npm packages
 npm update
-
-# Update git ssubmodule
-git submodule update --remote
